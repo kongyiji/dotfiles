9a8eab3e86118fb1bbbba49d1c0e6097c6628b30
kongyiji <xuyinghua1000@gmail.com>
2019-01-14 22:15:22 +0800

    add config folder into cmder folder

---
+++ cmder/config/user_profile.ps1
@@ -0,0 +1,28 @@
+# Use this file to run your own startup commands
+
+## Prompt Customization
+<#
+.SYNTAX
+    <PrePrompt><CMDER DEFAULT>
+    λ <PostPrompt> <repl input>
+.EXAMPLE
+    <PrePrompt>N:\Documents\src\cmder [master]
+    λ <PostPrompt> |
+#>
+
+[ScriptBlock]$PrePrompt = {
+
+}
+
+# Replace the cmder prompt entirely with this.
+[ScriptBlock]$CmderPrompt = {
+    if (Test-Path $PROFILE) {
+        . $PROFILE
+    }
+}
+
+[ScriptBlock]$PostPrompt = {
+
+}
+
+## <Continue to add your own>
