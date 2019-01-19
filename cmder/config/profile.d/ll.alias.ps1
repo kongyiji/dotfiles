3f348f7a738a24366f9809eaed973e0103292650
kongyiji <xuyinghua1000@gmail.com>
2019-01-19 15:10:58 +0800

    add powershell alias ls and ll

---
+++ cmder/config/profile.d/ll.alias.ps1
@@ -0,0 +1,6 @@
+# ls
+# Pre: Install Get-ChildItemColor
+# Install-Module Get-ChildItemColor (or Install-Module Get-ChildItemColor -Scope CurrentUser
+# Get-ChildItemColor Homepage: https://github.com/joonro/Get-ChildItemColor
+
+Set-Alias ll Get-ChildItemColor -option AllScope
---
+++ cmder/config/profile.d/ls.alias.ps1
@@ -0,0 +1,6 @@
+# ls
+# Pre: Install Get-ChildItemColor
+# Install-Module Get-ChildItemColor (or Install-Module Get-ChildItemColor -Scope CurrentUser
+# Get-ChildItemColor Homepage: https://github.com/joonro/Get-ChildItemColor
+
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
