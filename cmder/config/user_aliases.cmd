1ebd4de86c7eeb49fc38de05cc0db406dfae40b4
kongyiji <xuyinghua1000@gmail.com>
2019-08-06 13:59:52 +0800

    fix alias problem

--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -18,7 +18,7 @@ cmderr=cd /d "%CMDER_ROOT%"
 ll=ls -l --show-control-chars -F --color $*
 ss=set http_proxy=http://127.0.0.1:1080 && set https_proxy=http://127.0.0.1:1080
 uss=set http_proxy=&&set https_proxy=
-gss=git config --global http.proxy 'http://127.0.0.1:1080' && git config --global https.proxy 'http://127.0.0.1:1080'
+gss=git config --global http.proxy http://127.0.0.1:1080 && git config --global https.proxy http://127.0.0.1:1080
 ugss=git config --global --unset http.proxy && git config --global --unset https.proxy
 sgo=set GO111MODULE=on && set GOPROXY=https://goproxy.io
 usgo=set GO111MODULE=auto && set GOPROXY=
