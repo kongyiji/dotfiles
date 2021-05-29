5beb002cafabbd7c3458ac181b637b59076fa8a4
kongyiji <kongyiji.priv@gmail.com>
2021-05-30 00:01:14 +0800

    add alias:scc&scr

--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -24,3 +24,5 @@ usgo=set GO111MODULE=auto && set GOPROXY=
 ydl=youtube-dl.exe --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36" $*
 ydlp=youtube-dl.exe --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36" --proxy socks5://127.0.0.1:10808/ $*
 ydlsubp=youtube-dl.exe --all-subs --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36" --proxy socks5://127.0.0.1:10808/ $*
+scc=scoop config proxy "127.0.0.1:7890"
+scr=scoop config rm proxy
